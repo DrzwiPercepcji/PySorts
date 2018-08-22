8bb916c144fd0796e055d99414c26759a21b90f3
Szymon Nowak <szynow95@gmail.com>
2018-08-22 21:04:32 +0200

    Added Insertion Sort.

--- bubblesort.py
+++ bubblesort.py
@@ -3,14 +3,16 @@ def bubble_sort(data):
 		j = len(data) - 1
 		
 		while j > i:
-			if data[j] < data[j-1]:
+			if data[j] < data[j - 1]:
 				temp = data[j]
 				
-				data[j] = data[j-1]
-				data[j-1] = temp
+				data[j] = data[j - 1]
+				data[j - 1] = temp
 				
 				print("next step: " + str(data))
+				
 			j -= 1
+			
 	return data
 
 data = [10, 2, 3, 5, 6, 1, 8, 9]
---
+++ insertionsort.py
@@ -0,0 +1,24 @@
+def insertion_sort(data):
+	for i in range(len(data)):
+		
+		j = i - 1
+		
+		while j >= 0:
+			if data[j] > data[j + 1]:
+				temp = data[j];
+				
+				data[j] = data[j + 1];
+				data[j + 1] = temp;
+				
+				print("next step: " + str(data))
+				
+			else:
+				break
+			
+			j -= 1
+	
+	return data
+
+data = [10, 2, 3, 5, 6, 1, 8, 9]
+print("start: " + str(data))
+data = insertion_sort(data)
